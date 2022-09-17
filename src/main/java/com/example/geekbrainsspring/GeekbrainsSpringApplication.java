85a93f5c4f9373d32f9c17bc052438ceeb626016
saban <sabanashvili.mihail@yandex.ru>
2022-09-17 21:20:27 +0300

    git init

---
+++ src/main/java/com/example/geekbrainsspring/GeekbrainsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.geekbrainsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekbrainsSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeekbrainsSpringApplication.class, args);
+    }
+
+}
