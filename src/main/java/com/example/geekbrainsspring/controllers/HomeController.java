0f3e3326640164ef87ef44b0e4b1e4b889e4d4d5
saban <sabanashvili.mihail@yandex.ru>
2022-10-02 14:51:29 +0300

    перенос функциональности со spring+jsp на springboot+thymeleaf

--- src/main/java/com/example/geekbrainsspring/controllers/HomeController.java
+++ src/main/java/com/example/geekbrainsspring/controllers/HomeController.java
@@ -1,16 +1,18 @@
 package com.example.geekbrainsspring.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/home")
+@Controller
+@RequestMapping("/home")
 public class HomeController {
-
     @GetMapping(value = "/start")
     public String hello(Model model, @RequestParam String name) {
         model.addAttribute("name", name);
         return "index";
     }
+
 }
--- src/main/java/com/example/geekbrainsspring/controllers/StudentsController.java
+++ src/main/java/com/example/geekbrainsspring/controllers/StudentsController.java
@@ -4,10 +4,12 @@ import com.example.geekbrainsspring.entities.Student;
 import com.example.geekbrainsspring.entities.Students;
 import com.example.geekbrainsspring.service.StudentsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("/student")
+@Controller
+@RequestMapping("/student")
 public class StudentsController {
     @Autowired
     private StudentsService studentsService;
@@ -25,24 +27,6 @@ public class StudentsController {
         return "students";
     }
 
-
-    @GetMapping(path = "/showStudentById")
-    @ResponseBody
-    public Student showStudentById(Model uiModel, @RequestParam int id) {
-        return studentsService.getStudentById(id);
-    }
-
-    @GetMapping(path = "/showStudentById/{sid}")
-    @ResponseBody
-    public Student showStudentById(@PathVariable("sid") int id) {
-        return studentsService.getStudentById(id);
-    }
-
-    @PostMapping(path = "/setStudentById")
-    public void setStudent(@RequestBody Student student) {
-        System.out.println(student.getFirstName() + " " + student.getLastName() + " " + student.getCountry());
-    }
-
     @GetMapping("/showForm")
     public String showSimpleForm(Model uiModel) {
         Student student = new Student();
@@ -53,6 +37,7 @@ public class StudentsController {
 
     @GetMapping("/processForm")
     public String processForm(@ModelAttribute("student") Student student) {
+        System.out.println();
         return "student-form-result";
     }
 
